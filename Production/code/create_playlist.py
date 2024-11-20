b34b505dfeee33fff42470867ad0719ba72df26e
Gabriel Ferreira <gabrielvictor.gferreira@gmail.com>
2024-11-19 23:34:59 -0600

    App

--- Production/code/create_playlist.py
+++ Production/code/create_playlist.py
@@ -6,7 +6,7 @@ from spotipy.cache_handler import FlaskSessionCacheHandler
 
 client_id = "be1b6f758c9d48a7bc17d4542525840e"
 client_secret = "b5fee9ec62b84b5bbed44a16310f71c9"
-redirect_uri = "https://simplyfy-recommender-system.streamlit.app"
+redirect_uri = "https://simplyfy-recommender-system.streamlit.app/"
 # redirect_uri = "http://localhost:8501"
 scope = 'playlist-modify-public, playlist-modify-private'
 # scope = 'playlist-modify-public'
